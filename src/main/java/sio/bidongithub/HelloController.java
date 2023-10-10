cdcc6de346f4bc39fdc5fa661986fb6b3fde20ac
Agarram <garram.avi@ortmontreuil.fr>
2023-10-10 17:17:08 +0200

    modif

--- src/main/java/sio/bidongithub/HelloController.java
+++ src/main/java/sio/bidongithub/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick( ){
-        elcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
